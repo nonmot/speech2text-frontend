a420a97ee0079b821e817577c834b4382e67123d
Motoya Nonaka <Motoya.Nonaka@ibm.com>
2025-08-09 10:12:49 +0900

    Display loading while fetching model list

--- src/components/ModelList.tsx
+++ src/components/ModelList.tsx
@@ -7,25 +7,31 @@ interface Model {
 }
 
 const ModelList: React.FC = () => {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
   const [models, setModels] = useState<Model[]>([]);
 
   useEffect(() => {
     const data = async () => {
+      setIsLoading(true);
       const data = await getModelList();
       setModels(data);
+      setIsLoading(false);
     }
     data();
   }, []);
   
   return (
     <div>
-        {models && models.length > 0 ? (
+        {isLoading ? (
+          <p>Loading...</p>
+        ) : 
+          models && models.length > 0 ? (
           <select>
             {models.map((model, index) => (
               <option key={index} value={model.name}>{model.name}, {model.language}</option>
             ))}
           </select>
-        ): (
+          ): (
           <p>Sorry, Something went wrong.</p>
         )}
     </div>
