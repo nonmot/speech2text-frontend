3244824afcd09e39293621e1b064d9fcf74200f2
Motoya Nonaka <Motoya.Nonaka@ibm.com>
2025-08-16 11:35:13 +0900

    fix error in testing

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { MemoryRouter } from 'react-router-dom';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(
+    <MemoryRouter initialEntries={["/"]}>
+      <App />
+    </MemoryRouter>
+  );
   const linkElement = screen.getByText(/Speech To Text/i);
   expect(linkElement).toBeInTheDocument();
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 import Header from "./components/Header";
 import Footer from "./components/Footer";
@@ -11,12 +11,10 @@ function App() {
     <div className="flex flex-col min-h-screen bg-gray-100">
       <Header />
       <main className="flex-grow container mx-auto">
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Home />}/>
-            <Route path="*" element={<NoMatch/>}/>
-          </Routes>
-        </BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="*" element={<NoMatch/>}/>
+        </Routes>
       </main>
       <Footer />
     </div>
